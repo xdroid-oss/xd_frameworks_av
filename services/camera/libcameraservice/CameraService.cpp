6cc32b8f35b696740a2dd233604fe391685d5083
repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags <xyzuannihboss@gmail.com>
2023-04-07 09:35:49 +0000

    Revert "camera/media: Support legacy HALv1 camera in mediaserver"
    This reverts commit 0a55fd6388f8f4b11c266e6da30308e2e12290a0.

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1064,11 +1064,7 @@ int32_t CameraService::mapToInterface(StatusInternal status) {
 Status CameraService::initializeShimMetadata(int cameraId) {
     int uid = CameraThreadState::getCallingUid();
 
-#ifdef NO_CAMERA_SERVER
-    String16 internalPackageName("media");
-#else
     String16 internalPackageName("cameraserver");
-#endif
     String8 id = String8::format("%d", cameraId);
     Status ret = Status::ok();
     sp<Client> tmp = nullptr;
@@ -1149,9 +1145,7 @@ Status CameraService::getLegacyParametersLazy(int cameraId,
 static bool isTrustedCallingUid(uid_t uid) {
     switch (uid) {
         case AID_MEDIA:        // mediaserver
-#ifndef NO_CAMERA_SERVER
         case AID_CAMERASERVER: // cameraserver
-#endif
         case AID_RADIO:        // telephony
             return true;
         default:
@@ -1284,7 +1278,6 @@ Status CameraService::validateClientPermissionsLocked(const String8& cameraId,
                 clientName8.string(), clientUid, clientPid, cameraId.string());
     }
 
-#ifndef NO_CAMERA_SERVER
     // Make sure the UID is in an active state to use the camera
     if (!mUidPolicy->isUidActive(callingUid, String16(clientName8))) {
         int32_t procState = mUidPolicy->getProcState(callingUid);
@@ -1296,7 +1289,6 @@ Status CameraService::validateClientPermissionsLocked(const String8& cameraId,
                 clientName8.string(), clientUid, clientPid, cameraId.string(),
                 callingUid, procState);
     }
-#endif
 
     // If sensor privacy is enabled then prevent access to the camera
     if (mSensorPrivacyPolicy->isSensorPrivacyEnabled()) {
