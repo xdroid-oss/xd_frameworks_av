b225493b3fe9b0b26267e9f31fe443089b8e2bd8
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-02 19:40:25 +0900

    fixup! av: support per-app volume [1/3]
    * Apply per-app volume to portid_volume_management

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -5909,10 +5909,12 @@ PlaybackThread::mixer_state MixerThread::prepareTracks_l(
                                               * track->getAppVolume();
                     }
                 } else {
-                    if (track->isPlaybackRestricted() || track->getPortMute()) {
+                    if (track->isPlaybackRestricted() || track->getPortMute() ||
+                        track->isAppMuted()) {
                         volume = 0.f;
                     } else {
-                        volume = masterVolume * track->getPortVolume();
+                        volume = masterVolume * track->getPortVolume()
+                                              * track->getAppVolume();
                     }
                 }
                 handleVoipVolume_l(&volume);
@@ -6107,7 +6109,8 @@ PlaybackThread::mixer_state MixerThread::prepareTracks_l(
                 }
             } else {
                 v = masterVolume * track->getPortVolume();
-                if (track->isPlaybackRestricted() || track->getPortMute()) {
+                if (track->isPlaybackRestricted() || track->getPortMute() ||
+                    track->isAppMuted()) {
                     v = 0;
                 }
             }
@@ -6909,11 +6912,12 @@ void DirectOutputThread::processVolume_l(IAfTrack* track, bool lastTrack)
                                shaperVolume == 0.f,
                                /*muteFromPortVolume=*/false});
     } else {
-        if (mMasterMute || track->isPlaybackRestricted()) {
+        if (mMasterMute || track->isPlaybackRestricted() || track->isAppMuted()) {
             left = right = 0;
         } else {
             float typeVolume = track->getPortVolume();
-            const float v = mMasterVolume * typeVolume * shaperVolume;
+            float appVolume = track->getAppVolume();
+            const float v = mMasterVolume * typeVolume * shaperVolume * appVolume;
 
             if (left > GAIN_FLOAT_UNITY) {
                 left = GAIN_FLOAT_UNITY;
